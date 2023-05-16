153944505fd0bfcff6d1614fbcac579ecaf6b350
Lilly Liefers <lillyliefers@me.com>
2023-05-16 17:26:22 +0200

    To DO adding, not deleting

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const Tasks = require("../models/ToDo.model")
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
+
+router.get("/projects", (req, res, next) => {
+  Tasks.find ()
+  .then (response => {
+    res.status(201).json(response)
+  })
+  .catch (error => {
+    res.status.json("error")
+  })
+})
 
 module.exports = router;
